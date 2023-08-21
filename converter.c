86dabc9895b5a3eac20a08302184911d2cf523be
Stanwukong <stanleyajanaku@gmail.com>
2023-08-21 08:17:53 -0700

    Added prototypes for other specifiers

--- converter.c
+++ converter.c
@@ -3,15 +3,18 @@
 /**
  * conv - matches symbol with corresponding
  *	function
- * @symbol: conversion specifier symbol to match
+ * @letter: conversion specifier symbol to match
  *
  * Return: function pointer to function matched by letter
  */
 
-void (*conv(const char *symbol))(va_list arg)
+void (*conv(const char *letter))(va_list arg)
 {
 	format_spec func[] = {
-		{"c", print_c}
+		{"c", print_c},
+		{"s", print_s},
+		{"d", print_d},
+		{"i", print_i}
 	};
 	int i;
 
@@ -22,6 +25,6 @@ void (*conv(const char *symbol))(va_list arg)
 			return (func[i].func);
 		}
 	}
-	write(2, "Symbol not found", 17);
+	write(2, "Conversion specifier not found", 17);
 	exit(1);
 }
